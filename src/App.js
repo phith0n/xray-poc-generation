f9cee6b69ea31e1dafde6220707857d1941a8efe
phith0n <phith0n.ph2f@gmail.com>
2019-07-07 05:44:51 +0800

    initial project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Breadcrumb, Row, Col, Form, Input, Button } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Content, Footer } = Layout;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      poc: {
+        name: "",
+        rules: [],
+        detail: {},
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+          <Row gutter={16} type="flex">
+            <Col span={12} >
+              <Form>
+                <Form.Item label="POC名">
+                  <Input 
+                    placeholder="由数字、字母、短横线组成" 
+                    type="text" 
+                    value={this.state.poc.name} 
+                    onChange={e => this.setState({poc: {name: e.target.value}})} 
+                  />
+                </Form.Item>
+                <Form.Item label="Field B">
+                  <Input placeholder="input placeholder" />
+                </Form.Item>
+                <Form.Item>
+                  <Button type="primary" size="large">Submit</Button>
+                </Form.Item>
+              </Form>
+            </Col>
+            <Col span={12} >
+              Content
+            </Col>
+          </Row>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>©2019 XRay POC Generation</Footer>
+      </Layout>
+    );
+  }
 }
 
 export default App;
